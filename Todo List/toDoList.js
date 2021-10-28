08387d591c283986a7fa9a8ea433072f936f1798
bluestarburst <hargreaves.bryant@gmail.com>
2021-10-28 00:05:34 -0500

    stoarge

--- Todo List/toDoList.js
+++ Todo List/toDoList.js
@@ -2,16 +2,72 @@ let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
+function getList() {
+    var list = JSON.parse(localStorage.getItem("list"));
+    if (list == null) {
+        list = {};
+        setList({});
+    }
+    return list;
+}
+
+function setList(list) {
+    list = JSON.stringify(list);
+    localStorage.setItem("list", list);
+}
+
+function addItem(item) {
+    var list = getList();
+    var maxListSize = 3000;
+    var id = Math.floor(Math.random() * maxListSize);
+    while (list[id] != undefined) {
+        console.log("same")
+        id = Math.floor(Math.random() * maxListSize);
+    }
+    list[id] = item;
+    setList(list);
+    return id;
+}
+
+function removeItem(id) {
+    var list = getList();
+    delete list[id]
+    setList(list);
+}
+
 addToDoButton.addEventListener('click', function(){
+    var id = addItem(inputField.value);
     var paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
     paragraph.innerText = inputField.value;
+    paragraph.id = id;
     toDoContainer.appendChild(paragraph);
     inputField.value = " ";
     paragraph.addEventListener('click', function(){
         paragraph.style.textDecoration = "line-through";
     })
     paragraph.addEventListener('dblclick', function(){
+        removeItem(paragraph.id);
         toDoContainer.removeChild(paragraph);
+
     })
 })
+
+var list = getList();
+var keys = Object.keys(list);
+keys.forEach(key => {
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = list[key];
+    paragraph.id = key;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = " ";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        removeItem(paragraph.id);
+        toDoContainer.removeChild(paragraph);
+
+    })
+});
