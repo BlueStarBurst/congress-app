bb61286aa307015fe976046669e2ffafc8d6dd63
BlueStarBurst <hargreaves.bryant@gmail.com>
2022-01-31 20:48:29 -0600

    new

---
+++ app/app.js
@@ -0,0 +1,9 @@
+import React, { useRef, useState, useEffect } from 'react'
+import { render } from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+render((
+    <>
+        <h1>jdslfjl</h1>
+    </>
+), document.getElementById('root'))
